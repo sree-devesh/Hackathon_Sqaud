a9f0f447105048bdc6f585ca743d6667f0d2bc07
sree-devesh <syj6thcsnw@privaterelay.appleid.com>
2026-05-12 17:34:42 +0530

    test files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Hackathon_Sqaud
+Yo u r u n i ve r s i t y i s h o s t i n g
---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+using namespace std;
+
+void solve() {
+    long long n,m;
+    cin>> n>> m;
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
